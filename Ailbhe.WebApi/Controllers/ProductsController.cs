4842e65c9b0b4dafc2e9b07a18a6293d71b90e5a
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-12 00:01:05 +0800

    Added default API to retrieve product list.

---
+++ Ailbhe.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Ailbhe.WebApi.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Ailbhe.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            var repository = new ProductRepository();
+            return repository.Retrieve();
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
