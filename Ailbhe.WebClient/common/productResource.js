b6ddb9b26c4986b9a88fb7917c34349868d30a3b
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-15 11:35:09 +0800

    Called the Web API to save the data.

--- Ailbhe.WebClient/common/productResource.js
+++ Ailbhe.WebClient/common/productResource.js
@@ -9,7 +9,11 @@
                     productResource])
 
     function productResource($resource, appSettings) {
-        return $resource(appSettings.serverPath + "/api/products/:search");
+        return $resource(appSettings.serverPath + "/api/products/:id",
+                null,
+                {
+                    "update": { method: "PUT" }
+                });
     }
 }());
 
